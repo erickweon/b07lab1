05ca625c166b87099865810cd5372ae7a28e99ab
Eric Kweon <erickweon96@gmail.com>
2022-09-22 16:03:52 -0400

    Added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,41 @@
+public class Polynomial {
+	double [] coefficients;
+	public Polynomial() {
+		coefficients = new double[1];
+		coefficients[0] = 0;
+	}
+	public Polynomial(double[] c1) {
+		/*
+		coefficients = new double[c1.length()];
+		for(int i=0; i<c1.length(); i++)
+			coefficients[i] = c1[i]; */
+		coefficients = c1.clone();
+	}
+	public Polynomial add(Polynomial p) {
+		int i;
+		boolean pGreater = p.coefficients.length > coefficients.length;
+		int maxlength = Math.max(p.coefficients.length, coefficients.length);
+		int minlength = Math.min(p.coefficients.length, coefficients.length);
+		double [] newPoly = new double[maxlength];
+		for(i=0; i<minlength; i++)
+			newPoly[i] = coefficients[i] + p.coefficients[i];
+		if(pGreater) {
+			for(i=minlength; i<maxlength; i++)
+				newPoly[i] = p.coefficients[i];
+		}
+		else {
+			for(i=minlength; i<maxlength; i++)
+				newPoly[i] = coefficients[i];
+		}
+		return new Polynomial(newPoly);
+	}
+	public double evaluate(double x) {
+		double result = 0;
+		for(int i=0; i<coefficients.length; i++)
+			result += coefficients[i] * Math.pow(x, i);
+		return result;
+	}
+	public boolean hasRoot(double x) {
+		return (this.evaluate(x) == 0);
+	}
+}
